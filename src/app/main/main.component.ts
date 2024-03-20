a7d4ce794cd975de32ccc060da62d42da76b49e4
Muhammad Aizat <mohdaizat34@gmail.com>
2024-03-20 16:17:35 +0800

    Refined code, Delete when edit is empty, value is required when adding task, check when done will be sort at the bottom

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 interface Todo {
   id: number, 
   title: string,
+  check: number, // i use number because in localCompare function, i can't use boolean 
 }
 
 
@@ -35,21 +36,28 @@ export class MainComponent {
     this.todoList.push({
       id: this.currentID,
       title: value,
+      check: 0,  // 0 means uncheck
   });
   }
 
   ToggleEdit(id:number):void {
-    this.isEditing[id] = !this.isEditing[id]; //Bila user tekan Edit, 
+    this.isEditing[id] = !this.isEditing[id]; //Bila user tekan Edit,
+    
+    if (this.todoList[id].title == "") {
+      this.todoList.splice(id, 1);
+    }
   }
 
-  SaveEdit(id:number):void {
-    this.isEditing[id] = false; // Save edit by setting editing state to false
-  }
+  // SaveEdit(id:number):void {
+  //   this.isEditing[id] = false; // Save edit by setting editing state to false
+  // }
 
   DeleteTodo(value:number):void {
-    console.log("delete  todo")
-    const index = this.todoList.findIndex(todo => todo.id === value); // Find index of todo with given id
-    this.todoList.splice(index, 1); // Remove the todo at the found index
+    //console.log("delete  todo")
+    this.todoList = this.todoList.filter(todo => todo.id != value) // Find index of todo with given id
+    console.log(this.todoList)
+    //console.log(index) 
+    //this.todoList.splice(index, 1); // Remove the todo at the found index
   }
 
   SortTodo():void {
@@ -57,14 +65,27 @@ export class MainComponent {
 
     if (this.sortStatus == 0) { //if 0 ascending ikut letter 
       console.log('ascending')
-      this.todoList.sort((a, b) => a.title.localeCompare(b.title));
+      this.todoList.sort((a, b) => a.check - b.check ||  a.title.localeCompare(b.title));
       this.sortStatus = 1; 
 
     } else { //if 1 descending ikut letter
       console.log('descending')
       this.sortStatus = 0;
-      this.todoList.sort((a, b) => b.title.localeCompare(a.title));
+      this.todoList.sort((a, b) => a.check - b.check  ||  b.title.localeCompare(a.title));
     }
-    
+  }
+
+  CompleteTodo(id: number): void {
+    const todo = this.todoList.find(todo => todo.id === id);
+
+    if (todo) {
+      todo.check = 1
+      this.todoList.sort((a, b) => a.check - b.check );
+      // //debug
+      // for (let i = 0; i < this.todoList.length; i++) {
+      //   console.log(this.todoList[i])
+      // }
+    } 
+
   }
 }
